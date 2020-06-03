88f9f9c0c62526b49cd343e4f35a8ed855ab75f8
Shivam Mehrotra <smehro4@uic.edu>
2020-06-03 17:18:17 -0500

    fix: change index.js static resources to correct path

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var express = require('express');  
+var app = express();
+//Static resources server
+app.use(express.static(__dirname));
+var server = app.listen(8082, function () {  
+    var port = server.address().port;
+    console.log('Server running at port %s', port);
+});
