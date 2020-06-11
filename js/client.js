58f687497c2375b44bc12a558c7fbc5f545e4959
amaesa <ashleymsan@gmail.com>
2020-06-10 22:19:21 -0500

    stores player name in session storage

--- js/client.js
+++ js/client.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
+    /* Selects Chicken */
     $('ul.chicken-selection li').click(function() {
         $('.chicken-selection li').removeClass('selected');
         $(this).addClass('selected');
         selectedChicken = $(this).data('chicken');
     });
+
+    /* Sets Name and Starts Game */
     $("#join").click(function() {
         var prompt = $("#prompt")[0];
+        window.sessionStorage.setItem('name', $("#chicken-name").val());
         prompt.style.display = "none";
     })
 })
