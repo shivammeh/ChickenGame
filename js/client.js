31fecc6b6bcfdb3365a77ca7c85e59e4f1bc7bc3
Shivam Mehrotra <smehro4@uic.edu>
2020-06-10 20:18:19 -0500

    fix jquery to be used in javascript to select chickens

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('ul.chicken-selection li').click(function() {
-        $('.tank-selection li').removeClass('selected');
+        $('.chicken-selection li').removeClass('selected');
         $(this).addClass('selected');
         selectedChicken = $(this).data('chicken');
     });
