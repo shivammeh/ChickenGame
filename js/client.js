a282420bd1ba4df374c58c21f0f5f31dd53b42b2
Shivam Mehrotra <smehro4@uic.edu>
2020-09-20 19:42:07 -0500

    fix: chicken showing properly on canvas, chicken orientation

--- js/client.js
+++ js/client.js
@@ -1,3 +1,33 @@
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+      this.image = new Image();
+      this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+      ctx = currentGame.context;
+      if (type == "image") {
+        ctx.drawImage(this.image,
+          this.x,
+          this.y,
+          this.width, this.height);
+      } else {
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }
+    this.newPos = function() {
+         this.x += this.speedX;
+         this.y += this.speedY;        
+    }
+}
+
 var currentGame = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -18,53 +48,23 @@ var currentGame = {
         var myChicken;
         switch(window.sessionStorage.getItem('chicken-type')){
             case "1":
-                myChicken = this.component(30, 30, "img/chicken/white-chicken.png", 10, 120, "image");
+                myChicken = new component(30, 30, "img/chicken/white-chicken.png", 10, 120, "image");
                 break;
             case "2":
-                myChicken = this.component(30, 30, "img/chicken/red-chicken.png", 10, 120, "image");
+                myChicken = new component(30, 30, "img/chicken/red-chicken.png", 10, 120, "image");
                 break;
             case "3":
-                myChicken = this.component(30, 30, "img/chicken/yellow-chicken.png", 10, 120, "image");
+                myChicken = new component(30, 30, "img/chicken/yellow-chicken.png", 10, 120, "image");
                 break;
             default:
-                myChicken = this.component(30, 30, "img/chicken/white-chicken.png", 10, 120, "image");
+                myChicken = new component(30, 30, "img/chicken/white-chicken.png", 10, 120, "image");
         }
         return myChicken;
     },
-
-    component : function(width, height, color, x, y, type) {
-        this.type = type;
-        if (type == "image") {
-          this.image = new Image();
-          this.image.src = color;
-        }
-        this.width = width;
-        this.height = height;
-        this.speedX = 0;
-        this.speedY = 0;
-        this.x = x;
-        this.y = y;
-        this.update = function() {
-          ctx = currentGame.context;
-          if (type == "image") {
-            ctx.drawImage(this.image,
-              this.x,
-              this.y,
-              this.width, this.height);
-          } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-          }
-        }
-        // this.newPos = function() {
-        //     this.x += this.speedX;
-        //     this.y += this.speedY;        
-        // }
-      },
       
       updateGameArea : function(myChicken) {
           currentGame.clear();
-         // myChicken.newPos();
+          myChicken.newPos();
           myChicken.update();
     }
 }
