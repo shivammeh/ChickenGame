3c5f11abea98f708fde7d21d0810b187f0988818
Shivam Mehrotra <smehro4@uic.edu>
2020-09-22 22:21:35 -0500

    Fix: no more Href on chicken select, chicken movement (up/down).

--- js/client.js
+++ js/client.js
@@ -1,3 +1,54 @@
+var currentGame = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        let myChicken = this.initializeGame();
+        currentGame.updateGameArea(myChicken);
+        window.addEventListener('keydown', function(event) {
+            currentGame.key = event.key; // "ArrowUp", or "ArrowDown"
+            currentGame.updateGameArea(myChicken);
+        });
+        window.addEventListener('keyup', function(event) {
+            currentGame.key = false;
+            currentGame.updateGameArea(myChicken);
+        });
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    initializeGame : function() {
+        var myChicken;
+        switch(window.sessionStorage.getItem('chicken-type')){
+            case "1":
+                myChicken = new component(70, 60, "img/chicken/white-chicken.png", 10, 120, "image");
+                break;
+            case "2":
+                myChicken = new component(70, 60, "img/chicken/red-chicken.png", 10, 120, "image");
+                break;
+            case "3":
+                myChicken = new component(70, 60, "img/chicken/yellow-chicken.png", 10, 120, "image");
+                break;
+            default:
+                myChicken = new component(70, 60, "img/chicken/white-chicken.png", 10, 120, "image");
+        }
+        return myChicken;
+    },
+      
+    updateGameArea : function(myChicken) {
+        currentGame.clear();
+        myChicken.speedX = 0;
+        myChicken.speedY = 0;
+        if (currentGame.key && currentGame.key == "ArrowUp") {myChicken.speedY = -3; }
+        if (currentGame.key && currentGame.key == "ArrowDown") {myChicken.speedY = 3; }     
+        myChicken.newPos();
+        myChicken.update();
+    }
+}
+
 function component(width, height, color, x, y, type) {
     this.type = type;
     if (type == "image") {
@@ -25,75 +76,6 @@ function component(width, height, color, x, y, type) {
     this.newPos = function() {
          this.x += this.speedX;
          this.y += this.speedY;        
-    }
-}
-
-var currentGame = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        let myChicken = this.initializeGame();
-        this.interval = setInterval(this.updateGameArea(myChicken), 20);
-        
-        window.addEventListener('keydown', function(event) {
-            const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-        });
-    },
-
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-
-    initializeGame : function() {
-        var myChicken;
-        switch(window.sessionStorage.getItem('chicken-type')){
-            case "1":
-                myChicken = new component(70, 60, "img/chicken/white-chicken.png", 10, 120, "image");
-                break;
-            case "2":
-                myChicken = new component(70, 60, "img/chicken/red-chicken.png", 10, 120, "image");
-                break;
-            case "3":
-                myChicken = new component(70, 60, "img/chicken/yellow-chicken.png", 10, 120, "image");
-                break;
-            default:
-                myChicken = new component(70, 60, "img/chicken/white-chicken.png", 10, 120, "image");
-        }
-        return myChicken;
-    },
-      
-      updateGameArea : function(myChicken) {
-          currentGame.clear(); 
-          document.onkeydown = checkKey(myChicken);     
-          myChicken.newPos();
-          myChicken.update();
-    },
-    
-    checkKey : function(e, myChicken) {
-        myChicken.speedX = 0;
-        myChicken.speedY = 0;  
-        e = e || window.event;
-    
-        if (e.keyCode == '38') {
-            // up arrow
-            myChicken.speedX = -1;
-        }
-        else if (e.keyCode == '40') {
-            // down arrow
-            myChicken.speedX = 1; 
-        }
-        else if (e.keyCode == '37') {
-        // left arrow
-            myChicken.speedY = -1; 
-        }
-        else if (e.keyCode == '39') {
-        // right arrow
-            myChicken.speedY = 1; 
-        }
     }
 }
 
